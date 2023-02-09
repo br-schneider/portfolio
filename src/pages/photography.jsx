961636ba6fc319b673ff75173965bf9a9accd97e
Brett Schneider <brett.c.schneider@icloud.com>
2023-02-09 17:53:10 -0600

    downgrade next

--- src/pages/photography.jsx
+++ src/pages/photography.jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Image from 'next/future/image'
 import { SimpleLayout } from '@/components/SimpleLayout'
 import { useState } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
