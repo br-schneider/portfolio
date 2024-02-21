79f8d581324032afc8d8e20a763f71fb002b7d55
Brett Schneider <brett.c.schneider@icloud.com>
2024-02-21 11:09:55 -0600

    metadata cleanup

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -2,7 +2,6 @@ import Gallery from '@/components/custom/gallery'
 import { SimpleLayout } from '@/components/tailwind/simple-layout'
 import { getAllPhotos } from '@/lib/photos'
 import { Metadata } from 'next'
-import Head from 'next/head'
 
 export const metadata: Metadata = {
   title: 'Photography',
@@ -13,19 +12,13 @@ export default async function PhotographyPage() {
   const photos = await getAllPhotos()
 
   return (
-    <>
-      <Head>
-        <title>Photography - Brett Schneider</title>
-        <meta name="description" content="Here's my camera roll." />
-      </Head>
-      <SimpleLayout
-        title="Here's my camera roll."
-        intro="Most of these are from my DJI Mavic Pro. "
-      >
-        <div className="-mt-5">
-          <Gallery images={photos} />
-        </div>
-      </SimpleLayout>
-    </>
+    <SimpleLayout
+      title="Here's my camera roll."
+      intro="Most of these are from my DJI Mavic Pro. "
+    >
+      <div className="-mt-5">
+        <Gallery images={photos} />
+      </div>
+    </SimpleLayout>
   )
 }
