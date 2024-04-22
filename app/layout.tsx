e39270bc750e553b5d64f31fdd9965b0519f5da8
Brett Schneider <brett.c.schneider@icloud.com>
2024-04-22 12:33:00 -0500

    fix analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import { Layout } from '@/components/tailwind/layout'
 import '@/styles/tailwind.css'
-import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Metadata } from 'next'
+import Script from 'next/script'
 import { Providers } from './providers'
 
 export const metadata: Metadata = {
@@ -32,9 +32,13 @@ export default function RootLayout({
           data-domain="bretts.dev"
           src="https://www.tracklyze.com/insightCore.js"
         ></script>
+        <Script
+          defer
+          data-domain="bretts.dev"
+          src="https://plausible.io/js/script.js"
+        />
 
         <SpeedInsights />
-        <Analytics />
         <Providers>
           <div className="flex w-full">
             <Layout>{children}</Layout>
