11990fff3cbaa7e898b9201562824b41402ac77b
Brett Schneider <brett.c.schneider@icloud.com>
2024-06-30 20:47:57 -0500

    init parallax

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -1,4 +1,4 @@
-import Gallery from '@/components/custom/gallery'
+import { ParallaxScroll } from '@/components/custom/parallax-scroll'
 import { SimpleLayout } from '@/components/tailwind/simple-layout'
 import { getAllPhotos } from '@/lib/photos'
 import { Metadata } from 'next'
@@ -11,13 +11,17 @@ export const metadata: Metadata = {
 export default async function PhotographyPage() {
   const photos = await getAllPhotos()
 
+  const imageUrls = photos.map((image) => {
+    return `https://bretts.dev/drone/${image}`
+  })
+
   return (
     <SimpleLayout
       title="Here's my camera roll."
       intro="Most of these are from my DJI Mavic Pro. "
     >
       <div className="-mt-5">
-        <Gallery images={photos} />
+        <ParallaxScroll images={imageUrls} />
       </div>
     </SimpleLayout>
   )
